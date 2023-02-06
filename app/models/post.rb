5f6a99843bc92c7bd1612ca56b81296291bf1d8b
tadd581 <nguyendat58211@gmail.com>
2023-02-07 01:18:42 +0700

    feat: add navbar & css

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :parent, class_name: "Post", optional: true
-  has_many :posts, foreign_key: "parent_id"
+  has_many :posts, foreign_key: :parent_id
 
   after_create_commit { broadcast_append_to "posts" }
 end
