b15772eb5c2a3103fae196f18b6b53bcd19b94d8
tadd582 <nguyendat58211@gmail.com>
2023-02-14 00:49:53 +0700

    feat: upload image

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   belongs_to :parent, class_name: "Post", optional: true
   has_many :posts, foreign_key: :parent_id
 
+  has_one_attached :image
+
   after_create_commit { broadcast_append_to "posts" }
 
   def is_friend_post(current_user)
